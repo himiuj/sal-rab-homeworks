ff0df6f7d19d857776ad59a8c866003740215905
eduard sultangareev <den.blokho@gmail.com>
2022-10-23 17:40:56 +0000

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -19,35 +19,34 @@
 // например:
 // {
 //    "data": {
-//      "client": "Иван +7(987)65-43-210",
+//     "client": "Иван +7(987)65-43-210",
 //      "order": {
 //        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-//        "sum": 900
+//       "sum": 900
 //       },
 //       "goods": [
 //         {
-//           "title": "Пицца",
+//          "title": "Пицца",
 //           "count": 2
 //         }
 //      ]
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+function sendRequest(name, phone, {street, house, entrance, floor, flat}, goods, sum) {
+    let data = {
+        client: name + ' ' + phone,
+        order: {
+            address: 'ул. ' + street + ', дом ' + house + ', ' + entrance + ' подъезд, ' + floor + ' этаж' + ', кв ' + flat,
+            sum: sum
+        },
+        
+       goods:[]
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+    let countOfGoods = goods.length;
+    for (let i = 0; i < countOfGoods; i++) {
+    data.goods.push({title:goods[i].title, count: goods[i].count});
+}
+let jsonData = JSON.stringify({data});
+return jsonData
 }
